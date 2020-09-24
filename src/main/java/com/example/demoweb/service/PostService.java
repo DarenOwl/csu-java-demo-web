d93adcbfa6a3746e27dc5bbee7d64f55d0ff923b
Julia Andreeva <charly.fox@mail.ru>
2020-09-24 21:43:59 +0500

    Database

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,29 +1,30 @@
 package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
+import com.example.demoweb.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class PostService {
     List<Post> posts;
+    @Autowired
+    PostRepository postRepository;
 
     public PostService() {
-        this.posts = new LinkedList<>();
-        create("first post");
-        create("second post");
-        create("third post");
+        this.posts = new ArrayList<>();
     }
 
-    public List<Post> listAllPosts() {
-        return posts;
+    public Iterable<Post> listAllPosts() {
+        return postRepository.findAll();
     }
 
     public void create(String text) {
-        posts.add(new Post((long) posts.size(), text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
